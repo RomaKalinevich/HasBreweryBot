15dc1f369dae15e178ade880d40d8a95a087e555
ST_DOMAIN\Kalinevich_RS <Kalinevich_RS@st.by>
2025-04-15 12:04:12 +0300

    add base entities

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class User
+{
+	public Guid Id { get; set; }
+    public string TelegramId { get; set; } = null!;
+    public string FullName { get; set; } = null!;
+    public Role Role { get; set; }
+}
---
+++ Domain/Enums/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Enums;
+
+public enum Role
+{
+	Admin = 0,
+	Brewer = 1,
+	Driver = 2
+}
